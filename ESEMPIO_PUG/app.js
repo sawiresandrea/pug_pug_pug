10cf4c3c9026cfa076d3568af28c4294044a17d5
sawiresandrea <sawires.andrea@iisgalvanimi.edu.it>
2019-11-15 11:56:36 +0000

    errore con json esercizio 02.1

--- ESEMPIO_PUG/app.js
+++ ESEMPIO_PUG/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const people = require('./people.json');
 const app = express();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    res.render('index');
-    //res.send('Hello World!');
+    res.render('index', {
+        title:'Homepage',
+        people: people.profiles
+    });
 });
 
 app.listen(3000, function () {
