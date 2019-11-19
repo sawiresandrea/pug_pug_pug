03b78236b05c23d56f5680e0c844ee3f70c2d0da
sawiresandrea <sawires.andrea@iisgalvanimi.edu.it>
2019-11-19 23:19:23 +0000

    moving img folder and starting es 02.2

--- ESEMPIO_PUG/app.js
+++ ESEMPIO_PUG/app.js
@@ -12,6 +12,14 @@ app.get('/', function (req, res) {
     });
 });
 
+app.get('/profile', (req, res) => {
+  const person = people.profiles.find((p) => p.id === req.query.id);
+  res.render('profile', {
+    title: `About ${person.firstname} ${person.lastname}`,
+    person,
+  });
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
